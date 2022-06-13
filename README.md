c7c10dfe5000b6052245948c65d65e0272524fd3
JohnnyW10 <jw.wolan@gmail.com>
2022-06-14 00:18:49 +0200

    All task is completed. Mission successfull

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-recruitment task GravityGlobal na frontend developera
+Jan Wolan - recruitment task GravityGlobal na frontend developera
 
 If responsive of images doesn't work plz go to incognito mode and resize browser and then run this project 
+I saved the counter value in the localstorage and after press reset button you need to clikc ctrl + r to refresh localstorage and set the value for 0. Then it's work correctly.
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,39 @@
-var count = 0
+var count =  window.localStorage.getItem("count") || 0;
+
 
 function displayPopUp() {
   let popupBox = document.getElementById('popupWindow')
-  popupBox.style.display = "block";
-  return count++;
+  let Overlay = document.getElementById('Overlay')
+  const resetButton = document.getElementById('el')
+  let popup_message = document.getElementById('popup_message');
+  
+  window.localStorage.setItem("count", ++count);
+
+  if(count < 5) {
+    popup_message.innerHTML = `You  have clicked ${count} to related button`
+  } else if (count >= 5) {
+    popup_message.innerHTML = `You  have clicked ${count} to related button`
+    resetButton.innerHTML = `<button id="reset" onClick="resetCounter()">Reset</button>`
+  }
+  popupBox.style.display = 'block'
+  Overlay.style.display = 'block'
 }
 
 document.getElementById('btnPopUp').addEventListener('click', displayPopUp)
+
+function resetCounter() {
+  window.localStorage.setItem("count", 0);
+  console.log(window.localStorage.getItem("count"))
+  const resetButton = document.getElementById('el').innerHTML = ''
+}
+
+function closePopup() {
+  let popupBox = document.getElementById('popupWindow')
+  let Overlay = document.getElementById('Overlay')
+
+  popupBox.style.display = 'none'
+  Overlay.style.display = 'none'
+}
+
+document.getElementById('closeBtn').addEventListener('click', closePopup)
+document.getElementById('Overlay').addEventListener('click', closePopup)
